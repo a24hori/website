8a258f2b45e41b58aa6b78660a2bc922996adedc
Atsushi Hori <javi08na4@icloud.com>
2019-10-14 00:22:31 +0900

    blog added

--- out/_next/static/EG37BGSgW_uTU0_obnyiR/pages/index.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"5Yp1":function(n,i){},RNiq:function(n,i,o){"use strict";o.r(i);var t=o("q1tI"),w=o.n(t);o("5Yp1"),w.a.createElement},vlRD:function(n,i,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return o("RNiq")}])}},[["vlRD",1,0]]]);
