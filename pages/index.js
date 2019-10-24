22cd10723502924b512fb3917bcec5711ca0964e
Atsushi Hori <atsushihori@localhost.localdomain>
2019-10-24 09:07:45 +0900

    new post

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Index = () => (
 			<title>A24H | Top</title>
 		</Head>
 		<h1>A24H's website (under construction)</h1>
-		<p>現在ウェブサイト作成中ですm(_ _)m（2019年10月14日現在）</p>
+		<p>現在ウェブサイト作成中ですm(_ _)m（2019年10月21日現在）</p>
 	  </Layout>
 );
 
