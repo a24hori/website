9fde65033056c11bd370ddbb08a594da294e38dc
Keith-Campbell <javi08na4@icloud.com>
2019-11-03 23:03:10 +0900

    MathJax test

--- next.config.js
+++ next.config.js
@@ -7,4 +7,14 @@ module.exports = withMDX({
     return pathMap
   }
 })
-
+const MathJax = require('mathjax').init({
+  loader: {load: ['input/tex', 'output/svg']},
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    processEscapes: true
+  },
+  CommonHTML: { matchFontHeight: false }
+}).then((MathJax) => {
+  const svg = MathJax.tex2svg('\\frac{1}{x^2-1}', {display: true});
+  console.log(MathJax.startup.adaptor.outerHTML(svg));
+}).catch((err) => console.log(err.message));
