a30e40966bee6955c63c5b9661524d7fae5fe122
Keith-Campbell <javi08na4@icloud.com>
2021-10-19 22:29:18 +0900

    発表・表彰歴を更新

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ export default () => {
 				</li>
 				<li><b>研究発表</b>:
 					<ol>
+						<li><u>堀篤史</u>，山川雄也，山下信雄，確率変分不等式問題に対する分布的ロバスト期待残差最小化，<a href='https://orsj.org/kansai/2021/10/05/2021%e5%b9%b4%e5%ba%a6%e9%96%a2%e8%a5%bf%e6%94%af%e9%83%a8ssor/'>日本OR学会関西支部SSOR2021</a>，オンライン開催，2021年10月．（<b>優秀発表賞受賞</b>）</li>
 						<li><u>Hori, A.</u>, Yamakawa, Y., and Yamashita, N., Distributionally Robust Expected Residual Minimization for Stochastic Variational Inequality Problems. <a href='https://www.siam.org/conferences/cm/conference/op21'><i>SIAM Conference on Optimization (OP21)</i></a>, Virtual (CP2 Advances in Robust and Distributionally Robust Optimization), July, 2021.</li>
 						<li><u>堀篤史</u>，山川雄也，山下信雄，確率変分不等式問題に対する分布的ロバスト期待残差最小化，<a href='https://www.orsj.or.jp/nc/2021s/'>日本OR学会2021年春季研究発表会</a>，東京工業大学（オンライン），2021年3月．</li>
 						<li><u>堀篤史</u>，福島雅夫，マルチリーダー・フォロワーゲームに対するGauss–Seidel法，<a href='http://www.orsj.or.jp/~nc2018s/'>日本OR学会2018年春季研究発表会</a>，東海大学，2018年3月．</li>
@@ -31,7 +32,9 @@ export default () => {
 				</li>
 				<li><b>表彰歴</b>:
 					<ol>
+						<li>日本オペレーションズ・リサーチ学会関西支部 SSOR2021 優秀発表賞 受賞</li>
 						<li><a href='http://www.orsj.or.jp/whatisor/award6.html'>2018年度日本オペレーションズ・リサーチ学会学生論文賞 受賞</a></li>
+						<li><a href='http://www.orsj.or.jp/chubu/?p=2708'>第44回日本OR学会中部支部研究発表会 学生論文賞優秀賞 受賞</a></li>
 					</ol>
 				</li>
 			</ul>
